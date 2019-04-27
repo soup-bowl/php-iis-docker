1b69070c6472aa416f78eddbad3b62117f8129fb
soup-bowl <code@revive.today>
2019-04-28 00:24:44 +0100

    Changes.

--- Docker/setup.ps1
+++ Docker/setup.ps1
@@ -1,6 +1,7 @@
 Expand-Archive -Path c:\php.zip -DestinationPath C:\php
 Remove-Item c:\php.zip -Force
 Copy-Item C:\php\php.ini-production C:\php\php.ini
+setx PATH "$env:path;C:\php" -m
 
 C:\vcr15.exe /quiet /install
 dism.exe /Online /Enable-Feature /FeatureName:IIS-CGI /All
